67be01459f6c68e95e1878cd6852b6c425fec9c5
flopsi <florian.l.frauenfelder@gmail.com>
2020-06-25 17:16:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# VPython test-program
---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+Created on 23.06.2020
+
+@author: flori
+'''
+import vpython as vp
+import tkinter as tk
+vp.box(pos=vp.vector(1,0,0), size=vp.vector(.5,.3,.2), color=vp.color.red)
+root = tk.Tk()
+root.title("Guide")
+root.mainloop()
