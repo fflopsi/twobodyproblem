15502c23c5820df950d214e0f3c7d64a47eaa5d2
flopsi <florian.l.frauenfelder@gmail.com>
2020-06-29 15:37:57 +0200

    add functionality to define values to pass to vpython

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 '''
 Created on 23.06.2020
-
 @author: flori
 '''
 import vpython as vp
 import tkinter as tk
-vp.box(pos=vp.vector(1,0,0), size=vp.vector(.5,.3,.2), color=vp.color.red)
+
+def execute(r):
+    vp.box(size = vp.vector(0.5, 0.5, r), color = vp.color.red)
+
 root = tk.Tk()
 root.title("Guide")
+e = tk.Entry()
+b = tk.Button(command = lambda: execute(float(e.get())))
+
+e.pack()
+b.pack()
+
 root.mainloop()
