4a267f944c07b7548d0debe58c2a76c0800f3edb
flopsi <florian.l.frauenfelder@gmail.com>
2020-06-30 17:01:51 +0200

    replace Tkinter with PyQt5

--- test.py
+++ test.py
@@ -2,18 +2,32 @@
 Created on 23.06.2020
 @author: flori
 '''
+import sys
 import vpython as vp
-import tkinter as tk
+from PySide2.QtWidgets import *
 
 def execute(r):
     vp.box(size = vp.vector(0.5, 0.5, r), color = vp.color.red)
 
-root = tk.Tk()
-root.title("Guide")
-e = tk.Entry()
-b = tk.Button(command = lambda: execute(float(e.get())))
+class widget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
 
-e.pack()
-b.pack()
+        self.ok = QPushButton("ok")
+        self.e = QLineEdit("123")
 
-root.mainloop()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.ok)
+        self.layout.addWidget(self.e)
+        self.setLayout(self.layout)
+
+        self.ok.clicked.connect(lambda: self.do(float(self.e.text())))
+
+    def do(self, r):
+        vp.box(size = vp.vector(0.5, 0.5, r), color = vp.color.red)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget1 = widget()
+    widget1.show()
+    sys.exit(app.exec_())
