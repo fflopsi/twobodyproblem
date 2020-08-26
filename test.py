5ae6c69f00e982a07aa1cfeaa8c813e28c36cc54
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2020-08-26 16:16:02 +0200

    shorten ok button function, add comments and docstrings

--- test.py
+++ test.py
@@ -8,43 +8,45 @@ import vpython as vp
 from PyQt5 import uic, QtCore, QtGui, QtWidgets
 
 class Settings(QtWidgets.QMainWindow):
+    """window for settings, no functionality yet"""
     def __init__(self, *args, parent = None, **kwargs):
         super(Settings, self).__init__(*args, parent, **kwargs)
         settings = uic.loadUi("settings.ui", self)
 
 class Examples(QtWidgets.QMainWindow):
+    """window for presets"""
     def __init__(self, *args, parent = None, **kwargs):
         super(Examples, self).__init__(*args, parent, **kwargs)
         examples = uic.loadUi("examples.ui", self)
         self.actionVerlassen.triggered.connect(self.close)
         self.b_ok.clicked.connect(self.ok)
-        self.b_fill.clicked.connect(self.fill)
+        self.b_fill.clicked.connect(self.fill) # "Übernehmen" button
 
     def fill(self):
-        if data.MASS[str(self.choice_central.currentText())] <= data.MASS[str(self.choice_sat.currentText())]:
+        """fill entry window fields with selected values"""
+        global error
+        if data.MASS[str(self.choice_central.currentText())] < data.MASS[str(self.choice_sat.currentText())]: # if satellite mass is bigger than central mass
+            error = True
             msg = QtWidgets.QMessageBox()
             msg.setIcon(QtWidgets.QMessageBox.Critical)
             msg.setText("Wahl nicht möglich")
             msg.setWindowTitle("Fehler")
-            msg.exec()
+            msg.exec() # show an error message box
         else:
+            error = False
             window.central_mass.setText(str(data.MASS[str(self.choice_central.currentText())]))
             window.central_radius.setText(str(data.RADIUS[str(self.choice_central.currentText())]))
             window.sat_mass.setText(str(data.MASS[str(self.choice_sat.currentText())]))
             window.sat_radius.setText(str(data.RADIUS[str(self.choice_sat.currentText())]))
             window.distance.setText(str(data.DISTANCE[str(self.choice_central.currentText())][str(self.choice_sat.currentText())]))
     def ok(self):
-        if data.MASS[str(self.choice_central.currentText())] < data.MASS[str(self.choice_sat.currentText())]:
-            msg = QtWidgets.QMessageBox()
-            msg.setIcon(QtWidgets.QMessageBox.Critical)
-            msg.setText("Wahl nicht möglich")
-            msg.setWindowTitle("Fehler")
-            msg.exec()
-        else:
-            self.fill()
+        """perform fill() and close window if no error occurred"""
+        self.fill()
+        if not error:
             self.close()
 
 class MainWindow(QtWidgets.QMainWindow):
+    """the main window, or entry window"""
     def __init__(self, *args, parent = None, **kwargs):
         super(MainWindow, self).__init__(*args, parent, **kwargs)
         uic.loadUi("entry.ui", self)
@@ -58,6 +60,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actionEinstellungen.triggered.connect(self.settings.show)
 
     def read(self):
+        """make all the entered values accessible everywhere"""
         global central_mass
         central_mass = float(self.central_mass.text())
         global central_radius
@@ -70,6 +73,7 @@ class MainWindow(QtWidgets.QMainWindow):
         distance = float(self.distance.text()) + central_radius + sat_radius
 
     def clear_fields(self):
+        """delete all values in all fields"""
         self.central_radius.setText("")
         self.central_mass.setText("")
         self.sat_mass.setText("")
@@ -77,12 +81,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.distance.setText("")
 
     def open_vpython(self):
+        """open vpython window with entered values"""
         self.read()
         central = vp.sphere(radius = central_radius)
         sat = vp.sphere(pos = vp.vector(distance,0,0), radius = sat_radius, make_trail = True)
         # pointer = vp.arrow(pos = sat.pos - )
         x = 0
-        while x < 300:
+        while x < 300: # simple movement
             vp.rate(30)
             sat.pos += vp.vector(distance,0,0) / 300
             x += 1
