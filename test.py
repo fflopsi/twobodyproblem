2783fe6b6af9ce250e3229fabfdbb5b862962905
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2020-08-28 16:06:23 +0200

    move classes Settings and Examples to separate files

--- test.py
+++ test.py
@@ -3,47 +3,9 @@ Created on 23.06.2020
 @author: flori
 '''
 
-import sys, data
+import sys, data, examples, settings
 import vpython as vp
 from PyQt5 import uic, QtCore, QtGui, QtWidgets
-
-class Settings(QtWidgets.QMainWindow):
-    """window for settings, no functionality yet"""
-    def __init__(self, *args, parent = None, **kwargs):
-        super(Settings, self).__init__(*args, parent, **kwargs)
-        settings = uic.loadUi("settings.ui", self)
-
-class Examples(QtWidgets.QMainWindow):
-    """window for presets"""
-    def __init__(self, *args, parent = None, **kwargs):
-        super(Examples, self).__init__(*args, parent, **kwargs)
-        examples = uic.loadUi("examples.ui", self)
-        self.actionVerlassen.triggered.connect(self.close)
-        self.b_ok.clicked.connect(self.ok)
-        self.b_fill.clicked.connect(self.fill) # "Übernehmen" button
-
-    def fill(self):
-        """fill entry window fields with selected values"""
-        global error
-        if data.MASS[str(self.choice_central.currentText())] < data.MASS[str(self.choice_sat.currentText())]: # if satellite mass is bigger than central mass
-            error = True
-            msg = QtWidgets.QMessageBox()
-            msg.setIcon(QtWidgets.QMessageBox.Critical)
-            msg.setText("Wahl nicht möglich")
-            msg.setWindowTitle("Fehler")
-            msg.exec() # show an error message box
-        else:
-            error = False
-            window.central_mass.setText(str(data.MASS[str(self.choice_central.currentText())]))
-            window.central_radius.setText(str(data.RADIUS[str(self.choice_central.currentText())]))
-            window.sat_mass.setText(str(data.MASS[str(self.choice_sat.currentText())]))
-            window.sat_radius.setText(str(data.RADIUS[str(self.choice_sat.currentText())]))
-            window.distance.setText(str(data.DISTANCE[str(self.choice_central.currentText())][str(self.choice_sat.currentText())]))
-    def ok(self):
-        """perform fill() and close window if no error occurred"""
-        self.fill()
-        if not error:
-            self.close()
 
 class MainWindow(QtWidgets.QMainWindow):
     """the main window, or entry window"""
@@ -51,8 +13,8 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, parent, **kwargs)
         uic.loadUi("entry.ui", self)
         self.setWindowIcon(QtGui.QIcon("icon.gif"))
-        self.examples = Examples(parent = self)
-        self.settings = Settings(parent = self)
+        self.examples = examples.Examples(parent = self)
+        self.settings = settings.Settings(parent = self)
         self.b_ok.clicked.connect(self.open_vpython)
         self.b_reset.clicked.connect(self.clear_fields)
         self.actionListe_mit_Voreinstellungen.triggered.connect(self.examples.show)
@@ -60,7 +22,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actionEinstellungen.triggered.connect(self.settings.show)
 
     def read(self):
-        """make all the entered values accessible everywhere"""
+        """make all the entered values globally accessible"""
         global central_mass
         central_mass = float(self.central_mass.text())
         global central_radius
@@ -85,11 +47,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.read()
         central = vp.sphere(radius = central_radius)
         sat = vp.sphere(pos = vp.vector(distance,0,0), radius = sat_radius, make_trail = True)
-        # pointer = vp.arrow(pos = sat.pos - )
+        central_pointer = vp.arrow(axis = vp.vector(0,-(distance / 5),0), color = vp.vector(1,0,0)) # set pointer arrows to the objects because the scales are too large
+        central_pointer.pos = central.pos - central_pointer.axis
+        sat_pointer = vp.arrow(axis = vp.vector(0,-(distance / 5),0), color = vp.vector(1,0,0))
+        sat_pointer.pos = sat.pos - sat_pointer.axis
         x = 0
         while x < 300: # simple movement
             vp.rate(30)
             sat.pos += vp.vector(distance,0,0) / 300
+            sat_pointer.pos = sat.pos - sat_pointer.axis
             x += 1
 
 if __name__ == "__main__":
