7df36a101a41d34031b171609ae4de1eb183e1f1
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2020-09-01 17:31:49 +0200

    add default value autofill and restart functionality, better pointers

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-import sys, data, examples, settings
+import sys, data, examples, settings, os
 import vpython as vp
 from PyQt5 import uic, QtCore, QtGui, QtWidgets
 
@@ -17,18 +17,50 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actionEinstellungen.triggered.connect(self.settings.show)
         global central_mass_default
 
+    def restart(self):
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
     def read(self):
         """make all the entered values globally accessible"""
         global central_mass
-        central_mass = float(self.central_mass.text())
         global central_radius
-        central_radius = float(self.central_radius.text())
         global sat_mass
-        sat_mass = float(self.sat_mass.text())
         global sat_radius
-        sat_radius = float(self.sat_radius.text())
         global distance
-        distance = float(self.distance.text()) + central_radius + sat_radius
+        try:
+            float(self.central_mass.text())
+        except ValueError:
+            self.central_mass.setText(str(data.MASS["Erde"]))
+        finally:
+            central_mass = float(self.central_mass.text())
+
+        try:
+            float(self.central_radius.text())
+        except ValueError:
+            self.central_radius.setText(str(data.RADIUS["Erde"]))
+        finally:
+            central_radius = float(self.central_radius.text())
+
+        try:
+            float(self.sat_mass.text())
+        except ValueError:
+            self.sat_mass.setText(str(data.MASS["Sputnik 2"]))
+        finally:
+            sat_mass = float(self.sat_mass.text())
+
+        try:
+            float(self.sat_radius.text())
+        except ValueError:
+            self.sat_radius.setText(str(data.RADIUS["Sputnik 2"]))
+        finally:
+            sat_radius = float(self.sat_radius.text())
+
+        try:
+            float(self.distance.text())
+        except ValueError:
+            self.distance.setText(str(data.DISTANCE["Erde"]["Sputnik 2"]))
+        finally:
+            distance = float(self.distance.text()) + central_radius + sat_radius
 
     def clear_fields(self):
         """delete all values in all fields"""
@@ -45,12 +77,13 @@ class MainWindow(QtWidgets.QMainWindow):
         central = vp.sphere(radius = central_radius)
         sat = vp.sphere(pos = vp.vector(distance,0,0), radius = sat_radius, make_trail = True)
         central_pointer = vp.arrow(axis = vp.vector(0,-(distance / 5),0), color = vp.vector(1,0,0)) # set pointer arrows to the objects because the scales are too large
-        central_pointer.pos = central.pos - central_pointer.axis
+        central_pointer.pos = central.pos - central_pointer.axis + vp.vector(0,central_radius,0) # set central pointer to the outside of central
         sat_pointer = vp.arrow(axis = vp.vector(0,-(distance / 5),0), color = vp.vector(1,0,0))
-        sat_pointer.pos = sat.pos - sat_pointer.axis
         x = 0
         while x < 300: # simple movement
             vp.rate(30)
             sat.pos += vp.vector(distance,0,0) / 300
-            sat_pointer.pos = sat.pos - sat_pointer.axis
+            sat_pointer.pos = sat.pos - sat_pointer.axis + vp.vector(0,sat_radius,0)
             x += 1
+        if self.settings.do_restart.isChecked():
+            self.restart()
