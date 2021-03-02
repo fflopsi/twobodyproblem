dec55a3d5597a8ada74d2304e1a7346f23d22183
flopsi-l-f <florian.l.frauenfelder@gmail.com>
2021-03-02 15:13:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name="twobodyproblem",
-    version="0.2.0",
+    version="0.2.2",
     description="This is a little simulation for the gravitational two body problem.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Florian Frauenfelder",
     author_email="florian.l.frauenfelder@gmail.com",
     url="https://github.com/flopsi-l-f/two-body-problem_simulation",
     keywords="simulation, gravitation, pyside, vpython",
     python_requires=">=3.8",
-    package_data={"twobodyproblem": ["ui"]},
+    package_data={"twobodyproblem": ["ui/entry.ui", "ui/examples.ui", "ui/settings.ui", "saved_data/presets.yml"]},
     packages=["twobodyproblem", "twobodyproblem.visualization"],
     install_requires=[
         "pyyaml>=5.4.1",
