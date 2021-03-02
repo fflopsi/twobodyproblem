aeb658fce54ac3297dd8b6bff184c662486e729a
flopsi-l-f <florian.l.frauenfelder@gmail.com>
2021-03-02 19:45:02 +0100

    update setup.py to include icon.gif

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/flopsi-l-f/two-body-problem_simulation",
     keywords="simulation, gravitation, pyside, vpython",
     python_requires=">=3.8",
-    package_data={"twobodyproblem": ["ui/entry.ui", "ui/examples.ui", "ui/settings.ui", "saved_data/presets.yml"]},
+    package_data={"twobodyproblem": ["ui/icon.gif", "ui/entry.ui", "ui/examples.ui", "ui/settings.ui", "saved_data/presets.yml"]},
     packages=["twobodyproblem", "twobodyproblem.visualization"],
     install_requires=[
         "pyyaml>=5.4.1",
