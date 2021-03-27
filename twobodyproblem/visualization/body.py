87d05974f5d87ca01d669a8a0c46dda21dfdda1a
flopsi-l-f <florian.l.frauenfelder@gmail.com>
2021-03-27 17:24:20 +0100

    small improvements more comments and docstrings

--- twobodyproblem/visualization/body.py
+++ twobodyproblem/visualization/body.py
@@ -13,7 +13,7 @@ class Body(vp.sphere):
                  **kwargs):
         """constructor extends vpython.sphere constructor
 
-        args:
+        additional args:
             sim: simulation where the bodies are created in
             name: the name of the body
             mass: the mass of the body (default 1.0)
@@ -22,55 +22,63 @@ class Body(vp.sphere):
         super(Body, self).__init__(**kwargs)
         self.parent = sim
         self._name = name
-        self._velocity = velocity
         self._mass = mass
-        self._force = vp.vector(0, 0, 0)
-        self._acceleration = vp.vector(0, 0, 0)
+        self.velocity = velocity
+        self.force = vp.vector(0, 0, 0)
+        self.acceleration = vp.vector(0, 0, 0)
 
     @property
     def name(self):
+        """returns the name of the body"""
         return self._name
 
     @property
     def mass(self):
+        """returns the mass of the body"""
         return self._mass
 
     @property
     def force(self):
+        """returns the force of the body"""
         return self._force
 
     @force.setter
-    def force(self, value):
+    def force(self, value: vp.vector):
+        """set the force of the body"""
         if not isinstance(value, vp.vector):
             raise TypeError("force must be a vector")
         self._force = value
 
     @property
     def acceleration(self):
+        """returns the acceleration of the body"""
         return self._acceleration
 
     @acceleration.setter
-    def acceleration(self, value):
+    def acceleration(self, value: vp.vector):
+        """set the acceleration of the body"""
         if not isinstance(value, vp.vector):
             raise TypeError("acceleration must be a vector")
         self._acceleration = value
 
     @property
     def velocity(self):
+        """returns the velocity of the body"""
         return self._velocity
 
     @velocity.setter
-    def velocity(self, value):
+    def velocity(self, value: vp.vector):
+        """set the velocity of the body"""
         if not isinstance(value, vp.vector):
             raise TypeError("velocity must be a vector")
         self._velocity = value
 
-    def calculate(self, other, delta_t=1):
+    def calculate(self, other, delta_t=10):
         """calculate force, acceleration, velocity and position of both bodies
 
         args:
             other: the other body that should be calculated with
-            delta_t: Δt value (number of seconds in one calculation)
+            delta_t: Δt value (seconds in one calculation) (default 10)
         """
         # vector for distance
         r = other.pos - self.pos
