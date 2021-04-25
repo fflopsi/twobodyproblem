7a392d51119547c3791ec605456d92b4ddb482b2
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-04-25 12:31:27 +0200

    add usage of from_input methods add saving/loading values/options possibility

--- twobodyproblem/__main__.py
+++ twobodyproblem/__main__.py
@@ -42,132 +42,54 @@ if __name__ == "__main__":
           "parentheses () will be used.")
     print("The unit is indicated in brackets [] if needed.")
     print("For the inputs that are yes/no, type 1 for yes and 0 for no.")
-    print("\nFirst, you need to input the options:")
-    # default options
-    options = Options()
-    try:
-        options.canvas.width = int(input("canvas width (1000)[px]: "))
-    except ValueError:
-        pass
-    try:
-        options.canvas.height = int(input("canvas height (600)[px]: "))
-    except ValueError:
-        pass
-    print("color objects RGB: ")
-    try:
-        options.colors.bodies.x = int(input("\tR (255): "))
-    except ValueError:
-        pass
-    try:
-        options.colors.bodies.y = int(input("\tG (255): "))
-    except ValueError:
-        pass
-    try:
-        options.colors.bodies.z = int(input("\tB (255): "))
-    except ValueError:
-        pass
-    try:
-        options.pointers = bool(int(input("show the pointers (1): ")))
-    except ValueError:
-        pass
-    if options.pointers:
-        print("color pointers RGB: ")
-        try:
-            options.colors.pointers.x = int(input("\tR (255): "))
-        except ValueError:
-            pass
-        try:
-            options.colors.pointers.y = int(input("\tG (0): "))
-        except ValueError:
-            pass
-        try:
-            options.colors.pointers.z = int(input("\tB (0): "))
-        except ValueError:
-            pass
-    try:
-        options.rate = int(input("calculations per second (100): "))
-    except ValueError:
-        pass
-    try:
-        options.sim_time = int(input("simulation length (30)[s]: "))
-    except ValueError:
-        pass
-    try:
-        options.delta_t = int(input("acceleration factor (Δt) (10): "))
-    except ValueError:
-        pass
-    try:
-        options.central_centered = bool(int(
-            input("centered central body (0): ")))
-    except ValueError:
-        pass
-    try:
-        options.testing = bool(int(input("enable testing features (0): ")))
-    except ValueError:
-        pass
-    try:
-        options.restart = bool(int(
-            input("restart the program after a simulation (1): ")))
-    except ValueError:
-        pass
-    if args.debug:
-        print("options:", end=" ")
-        print(options.to_dict())
 
-    print("\nNext, you need to input the values:")
-    # default values
-    values = Values()
-    print("central body:")
-    try:
-        values.central.mass = float(input("\tmass (5.972e+24)[kg]: "))
-    except ValueError:
-        pass
-    try:
-        values.central.radius = float(input("\tradius (6371000)[m]: "))
-    except ValueError:
-        pass
-    print("\tstarting velocity [m/s]:")
-    try:
-        values.central.velocity.x = float(input("\t\tspeed in x (0): "))
-    except ValueError:
-        pass
-    try:
-        values.central.velocity.y = float(input("\t\tspeed in y (0): "))
-    except ValueError:
-        pass
-    try:
-        values.central.velocity.z = float(input("\t\tspeed in z (0): "))
-    except ValueError:
-        pass
-    print("satellite / second body:")
-    try:
-        values.sat.mass = float(input("\tmass (500)[kg]: "))
-    except ValueError:
-        pass
-    try:
-        values.sat.radius = float(input("\tradius (2)[m]: "))
-    except ValueError:
-        pass
-    print("\tstarting velocity [m/s]:")
-    try:
-        values.sat.velocity.x = float(input("\t\tspeed in x (0): "))
-    except ValueError:
-        pass
-    try:
-        values.sat.velocity.y = float(input("\t\tspeed in y (0): "))
-    except ValueError:
-        pass
-    try:
-        values.sat.velocity.z = float(input("\t\tspeed in z (-8000): "))
-    except ValueError:
-        pass
-    try:
-        values.distance = float(input("\tinitial distance (1000000)[m]: "))
-    except ValueError:
-        pass
+    open_val = input("\nIf you want to import values from a file, type y/Y: ")
+    if open_val == "y" or open_val == "Y":
+        path = input("Input the path to the file (if left empty, "
+                     "the standard file will be used): ")
+        if path == "":
+            values = Values.from_file()
+        else:
+            values = Values.from_file(path)
+    else:
+        print("\nFirst, you need to input the values:")
+        values = Values.from_input()
+        save_val = input("\nIf you want to save these values to a file, "
+                         "type y/Y: ")
+        if save_val == "y" or save_val == "Y":
+            path = input("Input the path to the file (if left empty, "
+                         "the standard file will be used): ")
+            if path == "":
+                values.save()
+            else:
+                values.save(path)
     if args.debug:
         print("values:", end=" ")
         print(values.to_dict())
+
+    open_opt = input("\nIf you want to import options from a file, type y/Y: ")
+    if open_opt == "y" or open_opt == "Y":
+        path = input("Input the path to the file (if left empty, "
+                     "the standard file will be used): ")
+        if path == "":
+            options = Options.from_file()
+        else:
+            options = Options.from_file(path)
+    else:
+        print("\nNext, you need to input the options:")
+        options = Options.from_input()
+        save_opt = input("\nIf you want to save these options to a file, "
+                         "type y/Y: ")
+        if save_opt == "y" or save_opt == "Y":
+            path = input("Input the path to the file (if left empty, "
+                         "the standard file will be used): ")
+            if path == "":
+                options = Options.from_file()
+            else:
+                options = Options.from_file(path)
+    if args.debug:
+        print("options:", end=" ")
+        print(options.to_dict())
 
     # create and start simulation
     Simulation(values=values, options=options).start()
